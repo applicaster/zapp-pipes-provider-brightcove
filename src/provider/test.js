92d9458a58f2e1e37b8768a9d5b5c65ac81e5ced
Yaniv Solnik <solnik@gmail.com>
2017-12-06 15:51:08 +0200

    fixed typo in package file

--- src/provider/test.js
+++ src/provider/test.js
@@ -1,4 +1,5 @@
 export const test = {
   testCommand:
-    'brightcove://fetchData?type=playlist&url=https://studio.brightcove.com/products/videocloud/media/playlists/5654175333001?client_id=fb1e962c-cdb2-4bb8-9c32-51dfb4d8067c&client_secret=BfuwAxmyCqpMc9ngkI8Qu4md2dcCl4IO1UGKlKiuHzebqg-dTDLat_43V6lHVQEY0YAZh-oLer60xsXOPAwgXQ&account_id=5653786027001'
+    'brightcove://fetchData?type=playlist&url=https://studio.brightcove.com/products/videocloud/media/playlists/5654175333001?client_id=fb1e962c-cdb2-4bb8-9c32-51dfb4d8067c&client_secret=BfuwAxmyCqpMc9ngkI8Qu4md2dcCl4IO1UGKlKiuHzebqg-dTDLat_43V6lHVQEY0YAZh-oLer60xsXOPAwgXQ&account_id=5653786027001',
+  requestMocks: []
 };
