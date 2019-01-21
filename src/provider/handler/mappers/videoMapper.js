c32424d9c893acae372a458778bf4e636b1ff594
Yaniv Solnik <solnik@gmail.com>
2019-01-21 18:03:55 +0200

    rounding ad position time to test if is postroll

--- src/provider/handler/mappers/videoMapper.js
+++ src/provider/handler/mappers/videoMapper.js
@@ -44,7 +44,7 @@ export function videoMapper(video) {
     video_ads = cue_points.map(cuepoint => {
       const { time, metadata: ad_url } = cuepoint;
       const offset =
-        time === 0 ? 'preroll' : time === duration ? 'postroll' : time;
+        time === 0 ? 'preroll' : Math.round(time) >= Math.round(duration) ? 'postroll' : time;
       return { offset, ad_url };
     });
   }
